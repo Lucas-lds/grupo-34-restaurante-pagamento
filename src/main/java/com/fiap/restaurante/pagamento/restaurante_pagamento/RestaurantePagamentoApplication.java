dfaa4fefa9d8b5848422dca966d4ee5fd0615bf1
jeang <jeangabriel180@hotmail.com>
2025-01-28 21:34:17 -0300

    initial commit

---
+++ src/main/java/com/fiap/restaurante/pagamento/restaurante_pagamento/RestaurantePagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.restaurante.pagamento.restaurante_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantePagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/restaurante/pagamento/restaurante_pagamento/RestaurantePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.restaurante.pagamento.restaurante_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
